f7b061d917c193a298233f88c2f12e45e61b30dc
seiferdisast <seifer.code@gmail.com>
2023-02-26 08:30:44 -0500

    request mapping

--- nasa-parts/src/main/java/co/seifer/nasaparts/HelloController.java
+++ nasa-parts/src/main/java/co/seifer/nasaparts/HelloController.java
@@ -1,11 +1,20 @@
 package co.seifer.nasaparts;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@RequestMapping("/greeting")
+
 public class HelloController {
     @GetMapping("/hello")
         public String hello() {
-            return "Hello World!";
-        } 
+            return "Hello World! 123";
+        }
+    @GetMapping("/bye")
+    public String bye() {
+        return "Bye cruel World! 123";
+    }
+    
 }
