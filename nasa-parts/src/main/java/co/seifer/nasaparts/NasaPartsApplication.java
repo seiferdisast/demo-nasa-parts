268a2a5d3a65932f3ca74fdb897063403c729d0d
seiferdisast <seifer.code@gmail.com>
2023-02-26 07:18:12 -0500

    agregate springboot project

---
+++ nasa-parts/src/main/java/co/seifer/nasaparts/NasaPartsApplication.java
@@ -0,0 +1,13 @@
+package co.seifer.nasaparts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaPartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaPartsApplication.class, args);
+	}
+
+}
---
+++ nasa-parts/src/test/java/co/seifer/nasaparts/NasaPartsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.seifer.nasaparts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaPartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
