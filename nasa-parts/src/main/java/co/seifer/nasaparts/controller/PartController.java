9d5abb7f3e2f9b336f66054ac5dc8b1151e57f33
seiferdisast <seifer.code@gmail.com>
2023-02-27 14:30:42 -0500

    delete and put methos of parts

--- nasa-parts/src/main/java/co/seifer/nasaparts/controller/PartController.java
+++ nasa-parts/src/main/java/co/seifer/nasaparts/controller/PartController.java
@@ -3,11 +3,11 @@ package co.seifer.nasaparts.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import co.seifer.nasaparts.model.Part;
 import co.seifer.nasaparts.repository.PartRepository;
-
 
 @RestController
 @RequestMapping("/part")
@@ -19,9 +19,28 @@ public class PartController {
     public Part createPart(@RequestBody Part part) {
         return partRepository.save(part);
     }
+
     @GetMapping
     public List<Part> getAllParts() {
         return partRepository.findAll();
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarElemento(@PathVariable Long id) {
+        // Código para eliminar el elemento con el id especificado
+        partRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> putResource(@RequestBody Part part, @PathVariable("id") Long id) {
+            partRepository.save(part);
+            return ResponseEntity.ok("resource saved");
+        }
+    /*
+     * public String name() {
+     * return "eliminado";
+     * }
+     */
 
 }
